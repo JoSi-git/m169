bbf0854809a401fa8da58137a48fd6674db08112
arkaizn <silas.gubler@hispeed.ch>
2025-05-14 11:14:00 +0200

    add port configuration and Title

--- install.sh
+++ install.sh
@@ -12,10 +12,17 @@ SCRIPT_DIR="$(pwd)"
 
 # Display title
 clear
-echo "#############################################"
-echo "#          Moodle Docker Installer          #"
-echo "#############################################"
-echo
+cat <<'EOF'
+
+----------------------------------------------------------------------------------------------------------
+  ___  ___    _   __  __              _ _       ___          _             _         _        _ _         
+ |   \/ __|_ | | |  \/  |___  ___  __| | |___  |   \ ___  __| |_____ _ _  (_)_ _  __| |_ __ _| | |___ _ _ 
+ | |) \__ \ || | | |\/| / _ \/ _ \/ _` | / -_) | |) / _ \/ _| / / -_) '_| | | ' \(_-<  _/ _` | | / -_) '_|
+ |___/|___/\__/  |_|  |_\___/\___/\__,_|_\___| |___/\___/\__|_\_\___|_|   |_|_||_/__/\__\__,_|_|_\___|_|
+
+----------------------------------------------------------------------------------------------------------
+
+EOF
 
 # Ask if system updates should be performed
 read -p "Do you want to perform system updates? (Y/n): " update_choice
@@ -88,6 +95,25 @@ fi
 
 
 # Changing port configuration
+# Backup und Anpassung von ports.conf
+cp /etc/apache2/ports.conf /etc/apache2/ports.conf.bak
+if ! grep -q '^Listen 8080' /etc/apache2/ports.conf; then
+  echo 'Listen 8080' >> /etc/apache2/ports.conf
+fi
+
+# Backup und Anpassung der VirtualHost-Konfiguration
+site_conf="/etc/apache2/sites-available/000-default.conf"
+cp "$site_conf" "${site_conf}.bak"
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:8080>/g" "$site_conf"
+
+# Backup und Anpassung von config.php
+moodle_cfg="/var/www/html/config.php"
+cp "$moodle_cfg" "${moodle_cfg}.bak"
+sed -i "s|\\$CFG->wwwroot\s*=.*|\\$CFG->wwwroot = 'http://localhost:8080';|g" "$moodle_cfg"
+
+systemctl reload apache2
+
+echo "Fertig: Apache lauscht nun auf 8080 und Moodle unter http://localhost:8080 erreichbar."
 
 
 # Final message and Docker Compose instructions (English)
