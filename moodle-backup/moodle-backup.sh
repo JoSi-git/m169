bb38887b329bd0ce9666dbfe8a19c8e90b018edb
Tronic_DK <david.kaestli@clavisit.com>
2025-05-25 17:02:10 +0200

    Changed Script to only make a full backup

--- moodle-backup/moodle-backup.sh
+++ moodle-backup/moodle-backup.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-#Variablen
+# Moodle Full Backup Script
 INSTALL_DIR="/opt/moodle-docker"
 BACKUP_DIR="$INSTALL_DIR/tools/moodle-backup"
 MOODLE_VERSION=$(sed -n "s/.*\$release *= *'\([0-9.]*\).*/\1/p" /var/www/html/version.php)
 TIMESTAMP=$(date "+%Y%m%d-%H%M")
+FILENAME="${MOODLE_VERSION}_${TIMESTAMP}_FULL.tar.gz"
 
-#Entscheidung Full oder Incremental
-MODE=""
+# Read .env for MySQL Password
+source "$INSTALL_DIR/.env"
 
-while getopts "fi" opt; do
-  case $opt in
-    f) MODE="FULL" ;;
-    i) MODE="INCREMENTAL" ;;
-    *) exit 1 ;;
-  esac
-done
+mkdir -p "$BACKUP_DIR"
 
-# Prüfen ob Modus gesetzt wurde
-if [[ -z "$MODE" ]]; then
-  exit 1
-fi
-
-FILENAME="${MOODLE_VERSION}_${TIMESTAMP}_${MODE}.tar.gz"
-
-#Datenbank Dump
+# Create DB Dump
 mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" moodle > "$BACKUP_DIR/db.sql"
 
-#Packen ins Archiv
+# Bundling
 tar -czf "$BACKUP_DIR/$FILENAME" -C /var/www/html . -C "$BACKUP_DIR" db.sql
 
-#Aufräumen
+# Remove Temp Dump-File
 rm "$BACKUP_DIR/db.sql"
